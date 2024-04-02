0b8f9c1b2f4b4e2bafb9d2c02cf47aba52b73e16
Manikanta M <manikanta.m@labsquire.com>
2024-04-03 01:05:00 +0530

    UI updared

--- data/data.js
+++ data/data.js
@@ -10,7 +10,7 @@ const App = new Vue({
         list: [
           {
             name: "var",
-            complete: false,
+            complete: true,
           },
           {
             name: "let",
@@ -25,27 +25,178 @@ const App = new Vue({
       {
         id: 2,
         name: "Operators",
-        completed: false,
+        complete: false,
         list: [
           {
             name: "Arthmetic",
-            completed: false,
+            complete: false,
           },
           {
             name: "Assignment",
-            completed: false,
+            complete: false,
           },
           {
             name: "Logical",
-            completed: false,
+            complete: false,
           },
           {
             name: "unary",
-            completed: false,
+            complete: false,
           },
           {
             name: "Ternary (Conditional)",
-            completed: false,
+            complete: false,
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Arrays",
+        complete: false,
+        list: [
+          {
+            name: "Creating an Array",
+            complete: true,
+          },
+          {
+            name: "Accessing Array Elements",
+            complete: true,
+          },
+          {
+            name: "Changing an Array Element",
+            complete: true,
+          },
+          {
+            name: "Converting an Array to a String",
+            complete: true,
+          },
+          {
+            name: "Access the Full Array",
+            complete: true,
+          },
+          {
+            name: "Arrays are Objects",
+            complete: false,
+          },
+          {
+            name: "Array Elements Can Be Objects",
+            complete: false,
+          },
+          {
+            name: "The length Property",
+            complete: false,
+          },
+          {
+            name: "Accessing the First Array Element",
+            complete: false,
+          },
+          {
+            name: "Accessing the Last Array Element",
+            complete: false,
+          },
+          {
+            name: "Looping Array Elements",
+            complete: false,
+          },
+          {
+            name: "Adding Array Elements",
+            complete: false,
+          },
+          {
+            name: "Associative Arrays",
+            complete: false,
+          },
+          {
+            name: "JavaScript new Array()",
+            complete: false,
+          },
+          {
+            name: "Array length",
+            complete: false,
+          },
+          {
+            name: "Array toString()",
+            complete: false,
+          },
+          {
+            name: "Array at()",
+            complete: false,
+          },
+          {
+            name: "Array join()",
+            complete: false,
+          },
+          {
+            name: "Array pop()",
+            complete: false,
+          },
+          {
+            name: "Array push()",
+            complete: false,
+          },
+          {
+            name: "Array shift()",
+            complete: false,
+          },
+          {
+            name: "Array unshift()",
+            complete: false,
+          },
+          {
+            name: "Array delete()",
+            complete: false,
+          },
+          {
+            name: "Array concat()",
+            complete: false,
+          },
+          {
+            name: "Array copyWithin()",
+            complete: false,
+          },
+          {
+            name: "Array flat()",
+            complete: false,
+          },
+          {
+            name: "Array splice()",
+            complete: false,
+          },
+          {
+            name: "Array toSpliced()",
+            complete: false,
+          },
+          {
+            name: "Array slice()",
+            complete: false,
+          },
+          {
+            name: "Array indexOf()",
+            complete: false,
+          },
+          {
+            name: "Array lastIndexOf()",
+            complete: false,
+          },
+          {
+            name: "Array includes()",
+            complete: false,
+          },
+          {
+            name: "Array find()",
+            complete: false,
+          },
+          {
+            name: "Array findIndex()",
+            complete: false,
+          },
+          {
+            name: "Array findLast()",
+            complete: false,
+          },
+          {
+            name: "Array findLastIndex()",
+            complete: false,
           },
         ],
       },
